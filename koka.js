617ed3efa38e460d36e1dad1262026651b3945c5
chiaki02dec <98389916+chiaki02dec@users.noreply.github.com>
2022-01-25 19:18:06 +0900

    re-compile

--- koka.js
+++ koka.js
@@ -1,33 +1,33 @@
-class koka {
+class Koka {
   constructor() {}
-  
+
   getInfo() {
     return {
-      id: 'koka',
-      name: 'koka',
+      id: 'Koka',
+      name: 'Koka',
       blocks: [
-          {
+        {
           opcode: 'power',
-          blocktype: Scratch.BlockType.REPORTER,
+          blockType: Scratch.BlockType.REPORTER,
           text: '[X]の[N]乗',
           arguments: {
             X: {
               type: Scratch.ArgumentType.NUMBER,
               defaultValue: '2'
-               },
+            },
             N: {
               type: Scratch.ArgumentType.NUMBER,
               defaultValue '3'
-               }
             }
-         }
+          }
+        }
       ]
     }
   }
-  
+
   power(args) {
     return Math.pow(args.X, args.N);
   }
 }
 
-Scratch.extensions.register(new koka());
+Scratch.extensions.register(new Koka());
